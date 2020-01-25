c40e1af40347619a1e200ccabe951cb5384b9497
Walker Mitchell <wmitchell17@mail.wou.edu>
2020-01-24 17:30:24 -0800

    Added SQL files for DB setup.

---
+++ DBDSwimTracker/DBD-SwimTracker/App_Data/down.sql
@@ -0,0 +1,15 @@
+﻿DROP TABLE [dbo].[Results] ;
+
+DROP TABLE [dbo].[Events] ;
+
+DROP TABLE [dbo].[TeamMeets] ;
+
+DROP TABLE [dbo].[Meets] ;
+
+DROP TABLE [dbo].[TeamRosters] ;
+
+DROP TABLE [dbo].[Teams] ;
+
+DROP TABLE [dbo].[Athletes] ;
+
+DROP TABLE [dbo].[Coaches] ;
