e7ad5125fb85c677c657b1ead82301b057c99b3b
Jacob Wiederhold <jwiederhold16@mail.wou.edu>
2020-02-01 10:58:20 -0800

    Generated project with user accounts

---
+++ DBDSwimTracker/DBD-Swim-Tracker-0.2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DBD_Swim_Tracker_0._2.Startup))]
+namespace DBD_Swim_Tracker_0._2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
