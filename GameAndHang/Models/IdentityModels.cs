230d7ee1bef132155d44045bb59b8a32cce8ab95
Jacob Wiederhold <jwiederhold16@mail.wou.edu>
2020-02-19 20:42:42 -0800

    Added new connection string

--- GameAndHang/Models/IdentityModels.cs
+++ GameAndHang/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace GameAndHang.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DatabaseConString", throwIfV1Schema: false)
         {
         }
 
