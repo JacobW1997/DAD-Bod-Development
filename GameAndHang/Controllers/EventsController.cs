8c5ca884cced7a0267e8c005846505f6953c6ece
kyle short <kshort2584@live.com>
2020-02-19 20:42:15 -0800

    Working on min/max feature

---
+++ GameAndHang/Controllers/EventsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GameAndHang.Models;
+
+namespace GameAndHang.Controllers
+{
+    public class EventsController : Controller
+    {
+        private GnHContext db = new GnHContext();
+
+        // GET: Events
+        public ActionResult Index()
+        {
+            var events = db.Events.Include(p => p.User);
+            return View(events.ToList());
+        }
+
+        // GET: Events/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event event1 = db.Events.Find(id);
+            if (event1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(event1);
+        }
+
+        // GET: Events/Create
+        public ActionResult Create()
+        {
+            ViewBag.HostID = new SelectList(db.Users, "ID", "CredentialsID");
+            return View();
+        }
+
+        // POST: Events/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,EventName,IsPublic,Date,EventDescription,EventLocation,PlayerSlotsMin,PlayerSlotsMax,UnsupGames,HostID")] Event event1)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Events.Add(event1);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.HostID = new SelectList(db.Users, "ID", "CredentialsID", event1.HostID);
+            return View(event1);
+        }
+
+        // GET: Events/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event event1 = db.Events.Find(id);
+            if (event1 == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.HostID = new SelectList(db.Users, "ID", "CredentialsID", event1.HostID);
+            return View(event1);
+        }
+
+        // POST: Events/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,EventName,IsPublic,Date,EventDescription,EventLocation,PlayerSlotsMin,PlayerSlotsMax,UnsupGames,HostID")] Event event1)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(event1).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.HostID = new SelectList(db.Users, "ID", "CredentialsID", event1.HostID);
+            return View(event1);
+        }
+
+        // GET: Events/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event event1 = db.Events.Find(id);
+            if (event1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(event1);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Event event1 = db.Events.Find(id);
+            db.Events.Remove(event1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ GameAndHang/Models/User.cs
@@ -0,0 +1,43 @@
+namespace GameAndHang.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class User
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public User()
+        {
+            EventPlayers = new HashSet<EventPlayer>();
+            Events = new HashSet<Event>();
+        }
+
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string CredentialsID { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string LastName { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime DOB { get; set; }
+
+        public virtual AspNetUser AspNetUser { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<EventPlayer> EventPlayers { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Event> Events { get; set; }
+    }
+}
