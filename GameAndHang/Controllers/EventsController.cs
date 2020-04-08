d8a6861c4e552fa43d1a487dc09f839b93e4dbf7
kyle short <kshort2584@live.com>
2020-04-08 10:30:42 -0700

    Finished feature to remove the ability to sign up for an event when the max players has been reached

--- GameAndHang/Controllers/EventsController.cs
+++ GameAndHang/Controllers/EventsController.cs
@@ -67,7 +67,7 @@ namespace GameAndHang.Controllers
             //+ System.Web.Configuration.WebConfigurationManager.AppSettings["GoogleAPIKey"].ToString() + "&callback=initMap";
             ViewBag.HostID = User.Identity.GetUserId();
             //ViewBag.ApiUrl = https://maps.googleapis.com/maps/api/js?key= + System.Web.Configuration.WebConfigurationManager.AppSettings["GoogleAPIKey"].ToString() + "&callback=initMap";
-            ViewBag.ApiUrl = "https://maps.googleapis.com/maps/api/js?key=" + System.Web.Configuration.WebConfigurationManager.AppSettings["GoogleAPIKey"].ToString() + "&callback=initMap";
+            ViewBag.ApiUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDuwWq60IrpVvV1uNd-1IvOmlAZ2tAGAM8";
             return View();
         }
 
@@ -80,17 +80,17 @@ namespace GameAndHang.Controllers
         {
             var currentID = User.Identity.GetUserId();
             AspNetUser currentUser = db.AspNetUsers.Find(currentID);
-           if(currentID == null || currentUser.EmailConfirmed == false)
-            {
-                ModelState.AddModelError("HostID", "Plese login before creating an event!");
-                return View(@event);
-            }
-           else if(currentUser.EmailConfirmed == false)
-            {
-                ModelState.AddModelError("HostID", "Plese confirm your email before creating an event!");
-                return View(@event);
-            }
-            else
+           //if(currentID == null || currentUser.EmailConfirmed == false)
+           // {
+           //     ModelState.AddModelError("HostID", "Plese login before creating an event!");
+           //     return View(@event);
+           // }
+           //else if(currentUser.EmailConfirmed == false)
+           // {
+           //     ModelState.AddModelError("HostID", "Plese confirm your email before creating an event!");
+           //     return View(@event);
+           // }
+            //else
             {
                 @event.HostID = currentID;
                 Guid g = Guid.NewGuid();
