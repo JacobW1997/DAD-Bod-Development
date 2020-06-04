0a1850b8964bc5cd55aacf50d344691c4f7b9f73
Jacob Wiederhold <jwiederhold16@mail.wou.edu>
2020-06-03 21:06:14 -0700

    Adding WIP file so Azure will deploy

---
+++ GameAndHang/UtilityFunctions/UtilityFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace GameAndHang.UtilityFunctions
+{
+    public class UtilityFunctions
+    {
+        public string SendRequest(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Accept = "application/json";
+            string jsonString = null;
+            using (WebResponse response = request.GetResponse())
+            {
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                jsonString = reader.ReadToEnd();
+                reader.Close();
+                stream.Close();
+            }
+            return jsonString;
+        }
+    }
+}
